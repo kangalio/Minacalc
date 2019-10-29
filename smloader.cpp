43ffef38f6ce9a7534a02b4ef71a01a0b0b92afb
RGates94 <rgates275@gmail.com>
2019-10-28 21:54:04 -0400

    Pull out notes block from full sm file

--- smloader.cpp
+++ smloader.cpp
@@ -3,21 +3,46 @@
 //
 
 #include <sstream>
+#include <iostream>
 #include "smloader.h"
 
 using std::stringstream;
 using std::string;
+SMNotes parse_main_block(stringstream&);
 
 SMNotes load_from_file(ifstream& file) {
     stringstream sm_buffer;
-    SMNotes output;
     sm_buffer << file.rdbuf();
+    string sm_text = sm_buffer.str();
+    SMNotes raw_block = std::vector<NoteInfo>();
+    while (!sm_text.empty()) {
+        size_t cool = sm_text.find('#');
+        if (cool == string::npos)
+            break;
+        sm_text = sm_text.substr(cool+1);
+        if (sm_text.substr(0,5) == "NOTES") {
+            for (int i = 0; i < 6; i++) {
+                cool = sm_text.find(':');
+                if (cool == string::npos)
+                    break;
+                sm_text = sm_text.substr(cool+1);
+            }
+            stringstream notes_block;
+            notes_block << sm_text.substr(sm_text.find('\n')+1, sm_text.find(';'));
+            raw_block = parse_main_block(notes_block);
+        }
+    }
+    return raw_block;
+}
+
+SMNotes parse_main_block(stringstream& sm_text) {
+    SMNotes output;
     int notes;
     int stupidname;
     float size = 0.f;
     float measurenumber = 0.f;
     std::vector<int> measure;
-    for (std::string line; std::getline(sm_buffer, line); )
+    for (std::string line; std::getline(sm_text, line); )
     {
         notes = 0;
         stupidname = 1;
@@ -46,6 +71,14 @@ SMNotes load_from_file(ifstream& file) {
         size += 1.f;
         measure.push_back(notes);
     }
+    float inside = 0.f;
+    for(int note_row : measure) {
+        if (note_row != 0) {
+            output.push_back(NoteInfo{note_row, measurenumber + inside / size});
+        }
+        inside += 1.f;
+    }
+    measure.clear();
 
     return output;
 }
