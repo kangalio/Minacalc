c1b8fda62ab5b4add2480e9d19b079d8e7d8eee8
RGates94 <rgates275@gmail.com>
2019-10-30 22:15:45 -0400

    Apply bpms to final output

--- smloader.cpp
+++ smloader.cpp
@@ -18,31 +18,45 @@ SMNotes load_from_file(ifstream& file) {
     SMNotes raw_block = std::vector<NoteInfo>();
     BPMs bpms = std::vector<BPM>();
     while (!sm_text.empty()) {
-        size_t cool = sm_text.find('#');
-        if (cool == string::npos)
+        size_t next_tag_position = sm_text.find('#');
+        if (next_tag_position == string::npos)
             break;
-        sm_text = sm_text.substr(cool+1);
+        sm_text = sm_text.substr(next_tag_position + 1);
         if (sm_text.substr(0,5) == "NOTES") {
             for (int i = 0; i < 6; i++) {
-                cool = sm_text.find(':');
-                if (cool == string::npos)
+                next_tag_position = sm_text.find(':');
+                if (next_tag_position == string::npos)
                     break;
-                sm_text = sm_text.substr(cool+1);
+                sm_text = sm_text.substr(next_tag_position + 1);
             }
             stringstream notes_block;
-            cool = sm_text.find(';');
-            notes_block << sm_text.substr(sm_text.find('\n')+1, cool);
+            next_tag_position = sm_text.find(';');
+            notes_block << sm_text.substr(sm_text.find('\n')+1, next_tag_position-1);
             raw_block = parse_main_block(notes_block);
-            sm_text = sm_text.substr(cool+1);
+            sm_text = sm_text.substr(next_tag_position + 1);
         } else if (sm_text.substr(0,4) == "BPMS") {
-            cool = sm_text.find(':');
-            sm_text = sm_text.substr(cool+1);
-            cool = sm_text.find(';');
+            next_tag_position = sm_text.find(':');
+            sm_text = sm_text.substr(next_tag_position + 1);
+            next_tag_position = sm_text.find(';');
             stringstream bpms_block;
-            bpms_block << sm_text.substr(0, cool);
+            bpms_block << sm_text.substr(0, next_tag_position);
             bpms = parse_bpms_block(bpms_block);
-            sm_text = sm_text.substr(cool+1);
+            sm_text = sm_text.substr(next_tag_position + 1);
         }
+    }
+    int next_bpm_index = 0;
+    float last_bpm = 120.f;
+    float last_bpm_time = 0.f;
+    float last_bpm_beat = 0.f;
+    for (NoteInfo& timestamp : raw_block) {
+        while (next_bpm_index < bpms.size() && bpms[next_bpm_index].beat <= timestamp.rowTime) {
+            last_bpm_time += (bpms[next_bpm_index].beat - last_bpm_beat) * 240.f / last_bpm;
+            last_bpm_beat = bpms[next_bpm_index].beat;
+            last_bpm = bpms[next_bpm_index].bpm;
+            next_bpm_index += 1;
+        }
+        timestamp.rowTime = last_bpm_time + (timestamp.rowTime - last_bpm_beat) * 240.f / last_bpm + 0.108f;
+        std::cout << timestamp.notes << " " << timestamp.rowTime << " " << last_bpm << std::endl;
     }
     return raw_block;
 }
@@ -100,7 +114,7 @@ BPMs parse_bpms_block(stringstream& bpms_block) {
     float next_bpm;
     BPMs bpm_list = std::vector<BPM>();
     while (bpms_block.rdbuf()->in_avail() && (bpms_block >> next_time) && (bpms_block.get() == '=') && (bpms_block >> next_bpm)) {
-        bpm_list.push_back(BPM{next_time, next_bpm});
+        bpm_list.push_back(BPM{next_time / 4.f, next_bpm});
         bpms_block.get();
         bpms_block.get();
     }
