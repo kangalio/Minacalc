28c4aa24a7b0163b4bc48b68f7b13990952f1295
RGates94 <rgates275@gmail.com>
2019-11-12 21:59:39 -0500

    Move printing logic into separate function

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 using std::cout;
 using std::endl;
 
+void printDifficulty(const DifficultyRating& rating) {
+    cout << "Overall: " << rating.overall << endl;
+    cout << "Stream: " << rating.stream << endl;
+    cout << "JumpStream: " << rating.jumpstream << endl;
+    cout << "HandStream: " << rating.handstream << endl;
+    cout << "Stamina: " << rating.stamina << endl;
+    cout << "Jackspeed: " << rating.jack << endl;
+    cout << "Chordjack: " << rating.chordjack << endl;
+    cout << "Technical: " << rating.technical;
+}
+
 std::vector<DifficultyRating> difficultyFromFile(const std::string& location) {
     std::ifstream sm_file;
     sm_file.open(location);
@@ -13,14 +24,6 @@ std::vector<DifficultyRating> difficultyFromFile(const std::string& location) {
         std::vector<std::vector<NoteInfo> > chart = load_from_file(sm_file);
         for (auto& difficulty : chart) {
             rating.push_back(MinaSDCalc(difficulty, 1.f, 0.93f));
-            cout << "Overall: " << rating.back().overall << endl;
-            cout << "Stream: " << rating.back().stream << endl;
-            cout << "JumpStream: " << rating.back().jumpstream << endl;
-            cout << "HandStream: " << rating.back().handstream << endl;
-            cout << "Stamina: " << rating.back().stamina << endl;
-            cout << "Jackspeed: " << rating.back().jack << endl;
-            cout << "Chordjack: " << rating.back().chordjack << endl;
-            cout << "Technical: " << rating.back().technical << endl << endl;
         }
     }
     else {
@@ -31,9 +34,14 @@ std::vector<DifficultyRating> difficultyFromFile(const std::string& location) {
 }
 
 int main(int argc, char *argv[]) {
+    std::vector<DifficultyRating> rating;
     if (argc > 1)
-        difficultyFromFile(argv[1]);
+        rating = difficultyFromFile(argv[1]);
     else
-        difficultyFromFile("../chart.sm");
+        rating = difficultyFromFile("../chart.sm");
+    for (auto& diff : rating) {
+        printDifficulty(diff);
+        cout << endl << endl;
+    }
     return 0;
 }
